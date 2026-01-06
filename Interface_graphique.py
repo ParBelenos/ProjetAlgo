4589bc7111ed1727c0a8d99b8f14625cbd62ddb5
ParBelenos <parbelenos125@gmail.com>
2026-01-06 21:11:34 +0100

    Base de l'interface

--- Interface_graphique.py
+++ Interface_graphique.py
@@ -1,44 +1,41 @@
-import pygame as p
-from random import randint
-from random import choice
+import pygame as pg
+import random as rd
 
-p.init()
+pg.init()
 
-size_x, size_y = 800, 470
+window = pg.display.set_mode((450, 550))
+font = pg.font.SysFont("arial", 36)
 
-window = p.display.set_mode([size_y, size_y])
+field = [[0, 0, 0, 0], 
+         [0, 0, 0, 0], 
+         [0, 0, 0, 0], 
+         [0, 0, 0, 0]]
 
 running = True
 
 while running:
-    for event in p.event.get():
-        if event.type == p.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
     window.fill("white")
     
     # CODE
     
-    a1 = p.draw.rect(window, "gray", (30, 30, 80, 80))
-    a2 = p.draw.rect(window, "gray", (140, 30, 80, 80))
-    a3 = p.draw.rect(window, "gray", (250, 30, 80, 80))
-    a4 = p.draw.rect(window, "gray", (360, 30, 80, 80))
+    zero_position = [
+        (r,c)
+        for r, row in enumerate(field)
+        for c, val in enumerate(row)
+        if val == 0
+    ]
 
-    b1 = p.draw.rect(window, "gray", (30, 140, 80, 80))
-    b2 = p.draw.rect(window, "gray", (140, 140, 80, 80))
-    b3 = p.draw.rect(window, "gray", (250, 140, 80, 80))
-    b4 = p.draw.rect(window, "gray", (360, 140, 80, 80))
-    
-    c1 = p.draw.rect(window, "gray", (30, 250, 80, 80))
-    c2 = p.draw.rect(window, "gray", (140, 250, 80, 80))
-    c3 = p.draw.rect(window, "gray", (250, 250, 80, 80))
-    c4 = p.draw.rect(window, "gray", (360, 250, 80, 80))
-    
-    d1 = p.draw.rect(window, "gray", (30, 360, 80, 80))
-    d2 = p.draw.rect(window, "gray", (140, 360, 80, 80))
-    d3 = p.draw.rect(window, "gray", (250, 360, 80, 80))
-    d4 = p.draw.rect(window, "gray", (360, 360, 80, 80))
-    
+    window.fill((158, 146, 135))
+
+
+    # Draw background tiles
+    for i in range(4):
+        for j in range(4):
+            pg.draw.rect(window, (205, 192, 180), (j*100+30, i*100+10, 90, 90), 0, 8)
 
 
 
@@ -51,8 +48,8 @@ while running:
 
 
 
-    p.display.flip()
+    pg.display.flip()
 
 
 
-p.quit()
+pg.quit()
