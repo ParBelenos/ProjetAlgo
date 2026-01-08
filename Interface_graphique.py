b987a24699f77b0c4d582da270e83c95a4126ee2
Nels-ai <smith.nelia.lmz@gmail.com>
2026-01-08 11:29:05 +0100

    fonction en def

--- Interface_graphique.py
+++ Interface_graphique.py
@@ -14,6 +14,18 @@ field = [[0, 0, 0, 0],
 show_start_text = True
 running = True
 
+# Draw background tiles
+def draw_background():
+    for i in range(4):
+        for j in range(4):
+            pg.draw.rect(window, (205, 192, 180), (j*100+30, i*100+10, 90, 90), 0, 8)
+
+# Draw start text
+def draw_start_text():
+    if show_start_text:
+        start_text = font.render("Press spacebar to start", True, "black")
+        window.blit(start_text, (80, 500))
+
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -33,16 +45,7 @@ while running:
     window.fill((158, 146, 135))
 
 
-    # Draw background tiles
-    for i in range(4):
-        for j in range(4):
-            pg.draw.rect(window, (205, 192, 180), (j*100+30, i*100+10, 90, 90), 0, 8)
-
-    # Draw start text
-    if show_start_text:
-        start_text = font.render("Press spacebar to start", True, "black")
-        window.blit(start_text, (20, 500))
-
+   
 
 
 
